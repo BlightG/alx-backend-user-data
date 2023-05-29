730b31fd8d2428b57692cf2cbba8565d279ea8c9
BlightG <birhane.gulilat@gmail.com>
2023-05-29 12:15:59 +0300

    task 4 first |=|) and change to auth.py import

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ a module for the Auth class """
-import request
+import requests
 from typing import List, TypeVar
 from flask import Flask
 
@@ -10,7 +10,16 @@ class Auth:
     """ a class to manage the API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ requires auth """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+
+        if path[-1] != '/':
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+        return True
+    
 
     def authorization_header(self, request=None) -> str:
         """ authorizes header """
