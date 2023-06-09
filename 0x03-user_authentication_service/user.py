b0e9f1c643f335182b1ced6ca335177f4df82bd4
BlightG <birhane.gulilat@gmail.com>
2023-06-09 08:14:19 +0300

    added a type for inheritance

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class User(Base):
+class User(Base: declarative_base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
